a746f7ac94e53021a7fe8e86c7acd2a5a46d154f
Marco Kuan <kuan.n.marco@gmail.com>
2020-09-01 17:20:25 -0700

    Include driver that uses Scanner as argument for class

---
+++ Driver.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Class that runs the converter. Asks for input and copy converts the files
+ * 
+ * @author Marco
+ */
+public class Driver {
+  /**
+   * The main method of the driver. Runs the converter with a Scanner.
+   * 
+   * @param args The arguments given in the terminal
+   */
+  public static void main(String[] args) {
+    // Converts the file
+    try (Scanner sc = new Scanner(System.in)) {
+      Converter convert = new Converter(sc);
+      convert.convertFiles();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    System.out.println("Copy conversion complete!");
+  }
+}
